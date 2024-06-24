b704545fad2081dfe32143eee99965faab10f1c6
Hugh Lyu <hugh.lv@gmail.com>
2024-06-24 23:20:47 +0800

    repeat the sample game

--- desktop/src/com/dustland/hellogdx/DesktopLauncher.java
+++ desktop/src/com/dustland/hellogdx/DesktopLauncher.java
@@ -8,8 +8,10 @@ import com.dustland.hellogdx.HelloGdx;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
 		config.setForegroundFPS(60);
-		config.setTitle("Hello GDX");
 		new Lwjgl3Application(new HelloGdx(), config);
 	}
 }
